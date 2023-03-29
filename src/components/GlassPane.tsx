278985ccbbc2cc8b4f93cf51482d3c81073673df
João Amaral <joao.amaral@ripio.com>
2023-03-28 21:44:53 -0300

    fixes GlassPane props type

--- src/components/GlassPane.tsx
+++ src/components/GlassPane.tsx
@@ -1,18 +1,18 @@
 import clsx from "clsx";
 import React from "react";
 
-export interface GlassPaneI {
-  children: React.ReactNode;
-  className: string;
-}
-
-const GlassPane: React.FC<GlassPaneI> = ({ children, className }) => {
+const GlassPane: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  className,
+  ...props
+}) => {
   return (
     <div
       className={clsx(
         "glass rounded-2xl border-solid border-2 border-gray-200",
         className
       )}
+      {...props}
     >
       {children}
     </div>
