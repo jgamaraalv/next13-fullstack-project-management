2374627ef5e4b92a318f067695923bb0db60e914
João Amaral <joao.amaral@ripio.com>
2023-03-29 15:24:07 -0300

    fetching projects data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
-import { db } from "@/lib/db";
 import { TASK_STATUS } from "@prisma/client";
+
+import { hashPassword } from "@/lib/auth";
+import { db } from "@/lib/db";
 
 const getRandomTaskStatus = () => {
   const statuses = [
@@ -18,7 +20,7 @@ async function main() {
       email: "user@email.com",
       firstName: "User",
       lastName: "Person",
-      password: "password",
+      password: await hashPassword("password"),
       projects: {
         create: new Array(5).fill(1).map((_, i) => ({
           name: `Project ${i}`,
