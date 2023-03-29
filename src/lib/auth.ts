4522a9eb2dcae2e3216728814aa894ff07c6f4fb
João Amaral <joao.amaral@ripio.com>
2023-03-29 12:29:29 -0300

    JSON web token

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,7 @@
 import bcrypt from "bcrypt";
+import { SignJWT, jwtVerify, JWTPayload } from "jose";
+import type { User } from "@prisma/client";
+import { db } from "./db";
 
 export const hashPassword = (password: string) => bcrypt.hash(password, 10);
 
@@ -6,3 +9,38 @@ export const comparePasswords = (
   plainTextPassword: string,
   hashedPassword: string
 ) => bcrypt.compare(plainTextPassword, hashedPassword);
+
+export const createJWT = (user: User) => {
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 60 * 60 * 24 * 7;
+
+  return new SignJWT({ payload: { id: user.id, email: user.email } })
+    .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+    .setExpirationTime(exp)
+    .setIssuedAt(iat)
+    .setNotBefore(iat)
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+};
+
+export const validateJWT = async (jwt: string) => {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+
+  return payload.payload as JWTPayload;
+};
+
+export const getUserFromCookie = async (cookies: any) => {
+  const jwt = cookies.get(process.env.COOKIE_NAME);
+
+  const { id } = await validateJWT(jwt.value);
+
+  const user = await db.user.findUnique({
+    where: {
+      id: id as string,
+    },
+  });
+
+  return user;
+};
