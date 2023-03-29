959a83663352a36c84cf8a9a39aa4b69fdb2361d
João Amaral <joao.amaral@ripio.com>
2023-03-29 11:17:31 -0300

    authentication handlers

---
+++ src/lib/api.ts
@@ -0,0 +1,46 @@
+import type { User } from "@prisma/client";
+
+interface FetcherParams {
+  url: string;
+  method: "POST" | "PUT" | "DELETE" | "GET";
+  body: Object;
+  json: boolean;
+}
+
+const fetcher = async ({ url, method, body, json = true }: FetcherParams) => {
+  const res = await fetch(url, {
+    method,
+    body: body && JSON.stringify(body),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("API Error");
+  }
+
+  if (json) {
+    const data = await res.json();
+    return data;
+  }
+};
+
+export const register = async (user: User) => {
+  return fetcher({
+    url: "/api/register",
+    method: "POST",
+    body: user,
+    json: false,
+  });
+};
+
+export const signin = async (user: User) => {
+  return fetcher({
+    url: "/api/signin",
+    method: "POST",
+    body: user,
+    json: false,
+  });
+};
