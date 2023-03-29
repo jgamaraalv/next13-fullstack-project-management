a48f0f636938d560d2fe7e3710938392a13f59ef
João Amaral <joao.amaral@ripio.com>
2023-03-29 13:39:48 -0300

    signin and signup fixes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,7 +10,7 @@ interface FetcherParams {
 const fetcher = async ({ url, method, body, json = true }: FetcherParams) => {
   const res = await fetch(url, {
     method,
-    body: body && JSON.stringify(body),
+    ...(body && { body: JSON.stringify(body) }),
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
